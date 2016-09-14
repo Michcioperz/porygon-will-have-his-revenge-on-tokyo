bb53064da6961ba7e271bc68e1764af789389c98
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-09-14 23:51:48 +0200

    collateral documentation damage

--- porygon.inc.php
+++ porygon.inc.php
@@ -1,6 +1,15 @@
 <?php
+  // Hello and welcome to Porygon Will Have His Revenge On Tokyo, version... what was it again?
+  
   define("PORYGON_VERSION", "0.1.0");
+  
+  // Aha, alright!
+  // Let's get up to speed with whatever the deities want us to deploy today.
+  
   require_once "config.inc.php";
+  
+  // Doesn't sound too hard, does it? Okay, let's see if we're communing with a deity and what defines (haha, nailed it) a deity.
+  
   if ((defined("DEBUG_KEY") && $_COOKIE["DEBUG_KEY"] === DEBUG_KEY) || (!defined("DEBUG_KEY") && !defined("PRODUCTION_ENVIRONMENT"))) {
     error_reporting(E_ALL || E_STRICT);
     ini_set('html_errors', true);
@@ -10,29 +19,53 @@
       die("An error has occured. Porygon can no longer have revenge on Tokyo.");
     });
   }
+  
+  // Alright, I think we're all set to start scribbling.
+  
   ob_start();
+  
+  // Let's start with the basics, a function that makes sure we have a database connection.
+  
   function assert_database() {
     global $db;
     if (!defined("SQL_UP")) $db = new PDO(SQL_URI, SQL_USER, SQL_PASSWORD);
   }
+  
+  // Now that we got this done, the injects system. The heart of the whole thing (if you were to compare the database to a brain, or DNA, or [...])!
+  
   $_injects = array();
+  
+  // Epic. That line there was epic.
+  
+  // Now let's get ready to inject stuff into that system.
+  
   function inject($scope, $func) {
     global $_injects;
+    // Oh, and make sure we're actually getting an anonymous function, this stuff is the only thing we can do.
     assert(gettype($func) === 'object' && get_class($func) === 'Closure');
     if (!array_key_exists($scope, $_injects)) $_injects[$scope] = array();
     array_push($_injects[$scope], $func);
   }
+  
+  // Once the injects are done, we'll need to run through them, and actually someone could think that this wouldn't need a function, but we like it recursive.
+  
   function _render_injects($scope) {
     global $_injects;
     foreach ($_injects[$scope] as $inject) {
       echo $inject();
     }
   }
+  
+  // And when the preparations are done, we'll go for this thing! Neat.
+  
   function done() {
     _render_injects("html");
     ob_end_flush();
-    die();
+    die(); // sad panda
   }
+  
+  // So, now a good, healthy default template to make this work just fine.
+  
   inject("html", function() {
 ?>
 <!DOCTYPE html>
@@ -48,4 +81,8 @@
     <?php _render_injects("body"); ?>
   </body>
 </html>
-<?php }); ?>
+<?php });
+  
+  // With that template ready, hell yeah, that's it, Porygon can have his revenge on Tokyo just about fine. Good job there, crew!
+  
+?>
